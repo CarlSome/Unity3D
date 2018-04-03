7708495ff2d4cda9ebe59274db28974674327c73
Carlsu <mrpuresu@gmail.com>
2018-04-03 21:15:44 +0800

    update

--- Homework2/Sun System simulator/Assets/RevolutionScript.cs
+++ Homework2/Sun System simulator/Assets/RevolutionScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class RevolutionScript : MonoBehaviour {
 
-    public Transform Center;
-    public float Speed;
+    public Transform Center;    //公转中心，默认为太阳的Position
+    public float Speed; //公转线速度
+    public float RotateSpeed;//自转角速度
+    //围绕旋转轴的x,y,z值
     public float X;
     public float Y;
     public float Z;
@@ -19,5 +21,6 @@ public class RevolutionScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.RotateAround(Center.position, Axis, Speed * Time.deltaTime);
-	}
+        transform.Rotate(Vector3.up * 360 * Time.deltaTime / RotateSpeed);
+    }
 }
